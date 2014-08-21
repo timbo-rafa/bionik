1b2c43e1fd980c7b9c017458d00f0d2957cab6bf
Rafael <timbo.rafa@gmail.com>
2014-08-21 17:35:18 -0400

    Passport Signup

--- app.js
+++ app.js
@@ -8,14 +8,19 @@
 var express = require('express');
 //var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var charts = require('./routes/chart/charts');
-		cloudant = require('./routes/database/cloudant');
+		cloudant = require('./app/database/cloudant');
+var passport = require('passport');
+		require('./app/passport/passport')(passport);
+
+var flash 	 = require('connect-flash');
 
 // setup middleware
 var app = express();
 
 app.use(express.cookieParser());
-app.use(express.session({secret: 'Figureoutabettersecretlater'}));
+app.use(express.session({secret: (process.env.SESSION_SECRET || 'My password is qwert1234')}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(bodyParser());
 app.use(app.router);
 app.use(express.favicon());
@@ -25,6 +30,8 @@ app.use(express.errorHandler());
 app.use(express.static(__dirname + '/public')); //setup static public directory
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views'); //optional since express defaults to CWD/views
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 
 // There are many useful environment variables available in process.env,
 // please refer to the following document for detailed description:
@@ -44,320 +51,11 @@ var host = (process.env.VCAP_APP_HOST || 'localhost');
 var port = (process.env.VCAP_APP_PORT || 3000);
 
     debug = (process.env.DEBUG || undefined);
+
+
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
 // Start server
 app.listen(port, host);
 cloudant.connect();
 console.log('App started on', host + ':' + port);
-
-// error/info messages
-msg = '';
-DATE_SEPARATOR = '/';
-
-//Date.prototype.toLocalISOString = function(){
-//	var d = new Date(this.getTime() - this.getTimezoneOffset() * 60 * 1000);
-//	return d.toISOString();
-//};
-//
-//Date.prototype.fromLocalISOString = function(){
-//	var d = new Date(this.getTime() + this.getTimezoneOffset() * 60 * 1000);
-//	return d;
-//};
-
-// function that generates error messages appropriately to be displayed on the browser.
-// related to the database.
-function errorMessage(err) {
-	console.log(err);
-	if (err.error == 'not_found') {
-		if ( err.reason == 'missing') {
-			return 'Database not found.';
-		} else return err.reason;
-	}
-
-	// as a last resort, return the error itself.
-	return err;
-};
-
-// Routing
-
-//debug
-app.get('*', function(req, res, next) {
-	//console.log(req.method, req.url, req.statusCode, req.httpVersion);
-	//console.log('req.body:',req.body, 'req.query:', req.query, 'req.params', req.params);
-	next();
-});
-
-//authentication and user session
-
-sessionInit = function(req, res, next) {
-  var userConfig = req.session.userConfig;
-  if (userConfig === undefined) {
-    userConfig = charts.newConfiguration();
-    //req.session.userConfig.defaultConfiguration();
-  }
-  if (typeof userConfig.starttime == 'string' || userConfig.starttime instanceof String)
-    userConfig.starttime = new Date(userConfig.starttime);
-  if (typeof userConfig.endtime == 'string' || userConfig.endtime instanceof String)
-    userConfig.endtime = new Date(userConfig.endtime);
-  //console.log('userConfig:', userConfig);
-	if (userConfig.starttime.toString() === "Invalid Date") {
-		console.log("Invalid Date for starttime");
-	}
-	if (userConfig.endtime.toString() === "Invalid Date") {
-		console.log("Invalid Date for endtime");
-	}
-  req.session.userConfig = userConfig;
-	console.log('session Init:',userConfig);
-	next();
-}
-
-function checkAuth(req, res, next) {
-  if (!req.session.user_id) {
-    res.send('You are not authorized to view this page');
-  } else {
-    next();
-  }
-}
-
-app.get('*', sessionInit); 
-app.post('*', sessionInit); 
-// first page
-
-app.get('/', function(req, res){
-	res.render('index'); 
-});
-
-app.get('/graphics', charts.example);
-app.get('/graphics/:patient', charts.summary);
-app.get('/charts', charts.example);
-app.get('/charts/:patient', charts.summary);
-//app.post('/config/charts/update', charts.update);
-
-
-// query data from the cloud
-app.get('/cloudant/query', function(req, res){
-	res.render('cloudant/query');
-});
-
-// post data to the cloudant db
-app.get('/cloudant/simulatedata', function(req, res) {
-	res.render('cloudant/simulatedata', {
-		now: new Date().toLocalISOString()
-	});
-});
-
-//retrieve data from form and redirect to appropriate page cloudant/:patient/:doc
-app.post('/cloudant/retrievepatient', function(req, res) {
-	res.redirect('/cloudant/' + req.body.querypatient + '/' + req.body.docTS);
-});
-
-//post the data from simulatedata and then render the display page(
-app.post('/cloudant/postsimulateddata', function(req, postres) {
-	console.log(req.body);
-	var patientname = req.body.PN;
-	var dbname = patientname;
-
-	//id of doc will be timestamp. Problematic if the mcu produced two sets of data with the same
-	//timestamp. Is that a problem??
-	console.log('time from HTML form');
-	req.body.TS = new Date(req.body.TS);
-	req.body.TS = req.body.TS.toISOString();
-	console.log(req.body.TS.toString());
-	req.body.time = new Date(req.body.TS).getTime();
-	console.log(req.body.time);
-	req.body.LS = parseInt(req.body.LS,10);
-	req.body.RS = parseInt(req.body.RS,10);
-	req.body.SS = parseInt(req.body.SS,10);
-	req.body.SU = parseInt(req.body.SU,10);
-	req.body.SD = parseInt(req.body.SD,10);
-	req.body.TN = parseInt(req.body.TN,10);
-
-	var docid = req.body.TS;
-	
-	cloudant.database(dbname);
-	var doc;
-	console.log('DEBUG');
-	console.log(req.body);
-
-	// check if patient database exists. If not, create it
-	// XXX: Handle db already exists error
-	cloudant.db.create(function(err, res) {
-		console.log("db.create(): err and res:", err, res);
-		//WARN: Existant document with same timestamp will be overwritten
-		cloudant.db.save(docid.toString(), {// the id of the document on the pacient's database
-				LS: req.body.LS,
-				RS: req.body.RS,
-				SS: req.body.SS,
-				SU: req.body.SU,
-				SD: req.body.SD,
-				TN: req.body.TN,
-				TS: req.body.TS.toString()
-			}, function (err, res) {
-				console.log("doc.save:");
-				console.log(err);
-				console.log(res);
-				if (err) {
-					msg = errorMessage(err);
-				} else {
-					msg = 'Document "' + docid +'" saved successfully.';
-					doc = res;
-				}
-				console.log(msg);
-				postres.redirect('/cloudant/' + dbname + '/' + docid);
-			});
-	});
-});
-
-//display all data from a patient
-app.get('/cloudant/:patient', function(req, res) {
-	var patient = req.params.PN;
-	var data = [];
-	cloudant.database(patient);
-	cloudant.db.all( function(err, clouddata) {
-		if (err) {
-			msg = errorMessage(err);
-			res.render('/cloudant/query', {
-				msg : msg
-			});
-			msg = '';
-		} else {
-			console.log('clouddata.length() = ' + clouddata.length);
-			clouddata.forEach(function(element, index, array) {
-				cloudant.database(patient);
-				cloudant.db.get(element, function(err, doc) {
-					//data.push(Object.clone(doc, true));
-					data.push(doc);
-					console.log(data.length);
-				
-					//last loop iteration
-					//this should be outside of db.get loop, but the data didnt hold the data (?)
-					if (data.length == clouddata.length) {
-						console.log('rendering. data.length ' + data.length);
-						res.render('cloudant/display', {
-							msg: msg,
-							data: data
-						});
-						msg = '';
-					}
-				});
-			});
-		}
-	});
-});
-
-//display one set of data from a patient
-app.get('/cloudant/:patient/:docid', function(req, res) {
-	var patient = req.params.patient;
-	var docid = req.params.docid;
-	cloudant.database(patient);
-	cloudant.db.get(docid, function(err, doc) {
-		if (err) {
-			msg = errorMessage(err);
-			res.render('cloudant/query', {
-				msg: msg,
-			});
-		} else {
-			console.log(doc);
-			res.render('cloudant/display', {
-				msg : msg,
-				data : [
-					{
-						RS : doc.RS,
-						LS : doc.LS,
-						SS : doc.SS,
-						SU : doc.SU,
-						SD : doc.SD,
-						TN : doc.TN,
-						time : doc.time,
-						TS : doc.TS
-					}
-				]
-			});
-		}
-		msg = '';
-	});
-});
-
-app.get('/_all_dbs', function(req, res) {
-	res.send({
-		"ok": true, 
-		"fake": true
-	});
-	res.render('index');
-});
-
-app.post('/:patient', function(req, apires) {
-	console.log('api saving data');
-	console.log(req.body, req.params);
-
-	req.body.TS = new Date(req.body.TS).toISOString();
-
-	cloudant.database(req.params.patient);
-	cloudant.db.create(function(err, res) {
-		//WARN: Existant document with same timestamp will be overwritten
-		cloudant.db.save(req.body.TS, req.body, function (err, res) {
-				console.log("api save:", req.body.TS);
-				console.log(err);
-				console.log(res);
-				if (err) {
-					msg = errorMessage(err);
-				} else {
-					msg = 'Document "' + req.body.TS +'" saved successfully.';
-					doc = res;
-				}
-				console.log(msg);
-				apires.send(res);
-			});
-	});
-});
-
-app.put('/:patient', function(req, res) {
-	res.send({"ok": true});
-	res.render('index');
-});
-
-// AJAX
-
-app.post('/api/ajax/updateCharts', function(req, res, next) {
-	console.log('updateCharts: ', req.method, req.url);
-	console.log(req.method, req.url);
-	charts.queryDataUpdate(req.session.userConfig, req.query);
-	charts.processData(req.session, function(err, chartData) {
-		if (err) {
-			res.send(err);
-		} else {
-			var formattedData = charts.formatForGoogleCharts(chartData, req.session.userConfig);
-
-			res.send({chartData: formattedData, chartConfig: req.session.userConfig});
-		}
-	});
-});
-
-//app.post('/api/ajax/updatePeriod', function(req, res, next) {
-//	console.log(req.method, req.url);
-//	charts.queryDataUpdate(req.session.userConfig, req.query);
-//	charts.processData(req.session, function(err, chartData) {
-//		if (err) {
-//			res.send(err);
-//		} else {
-//			var formattedData = charts.formatForGoogleCharts(chartData, req.session.userConfig);
-//			var options = {
-//				title: req.session.patient,
-//				vAxis: {
-//					title: req.session.userConfig.period,
-//					titleTextStyle: {color: "black"}
-//				},
-//				hAxis: {
-//					title: "Steps Accumulated",
-//					titleTextStyle: {color: "black"}
-//				}
-//			};
-//			res.send({chartData: formattedData, chartOptions: options});
-//		}
-//	});
-//});
-
-var n = 0;
-app.get('/api/ajax/configupdate', function(req, res) {
-	n = n + 1;
-	res.send(n.toString() + " clicks");
-});
--- routes/chart/methods.js
+++
@@ -1,6 +0,0 @@
-// This file holds the methods applied to charts to generate different kind of outputs
-
-exports.sum = function(displayObject, singledoc, action) {
-	  //console.log('Summing', displayObject[action], '+', singledoc.doc[action]);
-		  displayObject[action] += parseInt(singledoc.doc[action],10);
-};
